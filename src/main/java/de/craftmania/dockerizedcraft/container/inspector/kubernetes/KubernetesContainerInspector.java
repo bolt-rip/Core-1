6c4e8096f5a3ff7b1c3258457bf9717f2231e470
Indicado <indicado@walrus.gg>
2020-08-24 18:33:08 +0200

    Add option to load config from file
    Signed-off-by: Indicado <indicado@walrus.gg>

--- src/main/java/de/craftmania/dockerizedcraft/container/inspector/kubernetes/KubernetesContainerInspector.java
+++ src/main/java/de/craftmania/dockerizedcraft/container/inspector/kubernetes/KubernetesContainerInspector.java
@@ -1,12 +1,17 @@
 package de.craftmania.dockerizedcraft.container.inspector.kubernetes;
+
 import de.craftmania.dockerizedcraft.container.inspector.IContainerInspector;
 import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.config.Configuration;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.logging.Logger;
 
 public class KubernetesContainerInspector implements IContainerInspector {
@@ -15,11 +20,27 @@ public class KubernetesContainerInspector implements IContainerInspector {
     private Configuration configuration;
     private KubernetesClient client;
 
-    public KubernetesContainerInspector(Configuration configuration, ProxyServer proxyServer, Logger logger) {
+    public KubernetesContainerInspector(Configuration configuration, ProxyServer proxyServer, Logger logger,
+            File dataFolder) {
         this.proxyServer = proxyServer;
         this.logger = logger;
         this.configuration = configuration;
-        this.client = new DefaultKubernetesClient();
+
+        Config config = new ConfigBuilder().build();
+
+        if (!configuration.getBoolean("kubernetes.load-from-cluster")) {
+            try {
+                File file = new File(dataFolder, "kubeconfig.yml");
+                if (!file.exists())
+                    throw new IOException("Couldn't read file kubeconfig.yml");
+
+                config = Config.fromKubeconfig(new String(Files.readAllBytes(file.toPath())));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.client = new DefaultKubernetesClient(config);
     }
 
     public void runContainerInspection() {
@@ -29,7 +50,8 @@ public class KubernetesContainerInspector implements IContainerInspector {
     public void runContainerListener() {
         this.logger.info("[Kubernetes Container Inspector] Running listener.");
         String namespace = configuration.getString("kubernetes.namespace");
-        if(namespace == null ||namespace.isEmpty()) this.logger.severe("kubernetes.namespace not set.");
+        if (namespace == null || namespace.isEmpty())
+            this.logger.severe("kubernetes.namespace not set.");
         this.client.pods().inNamespace(namespace).watch(new PodWatcher(proxyServer, logger));
     }
 }
